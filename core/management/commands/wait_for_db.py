9865630b6477d924faf9d7d87130ce474847fea0
Manoj <madhikari@power-guard.jp>
2024-05-12 16:24:33 +0900

    adding project with docker

---
+++ core/management/commands/wait_for_db.py
@@ -0,0 +1,22 @@
+"""
+Django command to wait for database to be avaliable
+"""
+import time
+from psycopg2 import OperationalError as Psycopg2upError
+from django.db.utils import OperationalError
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    """Django Command to wait for database"""
+    def handle(self, *args, **options):
+        self.stdout.write('Waiting for database')
+        db_up = False
+        while db_up is False:
+            try:
+                self.check(databases=['default'])
+                db_up = True
+            except (Psycopg2upError, OperationalError):
+                self.stdout.write('Database unavaliable, waiting 1 second ....')
+                time.sleep(1)
+        self.stdout.write(self.style.SUCCESS('Database avaliable !'))
